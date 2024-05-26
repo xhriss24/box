79818bc05047e74a95567c4df681aa15119c0d3d
Astropeda <astromedia0010@gmail.com>
2024-05-26 18:37:48 +0100

    hd maker and removed rubbish

--- plugins/ai.js
+++ plugins/ai.js
@@ -52,7 +52,7 @@ async function processing(msg, data) {
 }
 UserFunction(
   {
-    cmdname: "remini",
+    cmdname: "hd",
     desc: "enhance image quality!",
     type: "ai",
     filename: __filename,
@@ -73,97 +73,6 @@ UserFunction(
         error,
         "*Process Denied :(*"
       );
-    }
-  }
-);
-UserFunction(
-  {
-    cmdname: "dehaze",
-    desc: "enhance image quality!",
-    type: "ai",
-    filename: __filename,
-  },
-  async (message) => {
-    let match = message.image ? message : message.reply_message;
-    if (!match || !match.image) {
-      return await message.send("*Reply to image, to enhance quality!*");
-    }
-    try {
-      let file = await match.download();
-      const images = await processing(file, "dehaze");
-      await message.send(images, {}, "img", message);
-      file = false;
-    } catch (error) {
-      message.error(
-        error + "\n\nCommand: dehaze",
-        error,
-        "*Process Denied :(*"
-      );
-    }
-  }
-);
-UserFunction(
-  {
-    cmdname: "recolor",
-    desc: "enhance image quality!",
-    type: "ai",
-    filename: __filename,
-  },
-  async (message) => {
-    let match = message.image ? message : message.reply_message;
-    if (!match || !match.image) {
-      return await message.send("*Reply to image, to enhance quality!*");
-    }
-    try {
-      let file = await match.download();
-      const images = await processing(file, "recolor");
-      await message.send(images, {}, "img", message);
-      file = false;
-    } catch (error) {
-      message.error(
-        error + "\n\nCommand: recolor",
-        error,
-        "*Process Denied :(*"
-      );
-    }
-  }
-);
-UserFunction(
-  {
-    pattern: "gpt4",
-    desc: "Get a response from ChatGPT-4.",
-    category: "ai",
-    filename: __filename,
-    use: "<query>",
-  },
-  async (m, query) => {
-    try {
-      if (!query) {
-        return await m.send("*_Please provide a query for ChatGPT-4!_*");
-      }
-
-      const apiUrl = `https://api.maher-zubair.tech/ai/chatgptv4?q=${encodeURIComponent(
-        query
-      )}`;
-      const response = await fetch(apiUrl);
-
-      if (!response.ok) {
-        return await m.send(
-          `*_Error: ${response.status} ${response.statusText}_*`
-        );
-      }
-
-      const data = await response.json();
-
-      if (data.status !== 200) {
-        return await m.send("*_An error occurred while fetching the data._*");
-      }
-
-      const reply = data.result;
-
-      await m.send(reply);
-    } catch (e) {
-      await m.error(`${e}\n\ncommand: gpt4`, e);
     }
   }
 );
@@ -212,46 +121,6 @@ UserFunction(
       );
     } catch (e) {
       await m.error(`${e}\n\ncommand: leapai`, e);
-    }
-  }
-);
-UserFunction(
-  {
-    pattern: "blackbox",
-    desc: "Get a response from Blackbox AI.",
-    category: "ai",
-    filename: __filename,
-    use: "<query>",
-  },
-  async (m, query) => {
-    try {
-      if (!query) {
-        return await m.send("*_Please provide a query for Blackbox AI!_*");
-      }
-
-      const apiUrl = `https://api.maher-zubair.tech/ai/blackbox?q=${encodeURIComponent(
-        query
-      )}`;
-      const response = await fetch(apiUrl);
-
-      if (!response.ok) {
-        return await m.send(
-          `*_Error: ${response.status} ${response.statusText}_*`
-        );
-      }
-
-      const data = await response.json();
-
-      if (data.status !== 200) {
-        return await m.send("*_An error occurred while fetching the data._*");
-      }
-
-      const result = data.result;
-      const responseMessage = result.split("$@$").pop().trim();
-
-      await m.send(responseMessage);
-    } catch (e) {
-      await m.error(`${e}\n\ncommand: blackbox`, e);
     }
   }
 );
